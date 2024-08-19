d857b2a5d29e882f56c1f0fcdffc41850e0ba372
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2024-08-19 20:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	Person(name, age){
+		this.name = name
+		this.age = age
+	}
+	function getName(){
+		return this.name
+	}
+	function setAge(age){
+		this.age = age
+	}	
+}
 
-class Student extends Person {}
+class Student extends Person {
+	function study(){
+		console.log(this.getName() + "is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	function teach(){
+		console.log(this.getName() + "is teaching")
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
